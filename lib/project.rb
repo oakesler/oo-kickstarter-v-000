456ff3b748cc4e0577af386bbc70f2f33395bea6
Olivia Kesler <oakesler@protonmail.ch>
2019-08-27 19:48:22 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+require "pry"
+
+class Project
+  
+  attr_reader :title, :backers
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(name)
+    @backers << name
+  end
+    
+end
